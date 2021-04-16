5b88fe822ab0fd0a68c7ee664a00e17dfb37250b
iamhyc <sudofree@163.com>
2021-04-16 11:04:20 +0800

    add run hint in traffic script

--- traffic/scripts/convert_main.py
+++ traffic/scripts/convert_main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 from sys import argv
 from pathlib import Path
+import subprocess as sp
 import create_sumo_vtypes as vtypes
 import netconvert_carla as net
 
-MAP_NAME = argv[1]
+#hey, see my humorous!
+Input_town_map_name_from = lambda x:x
+MAP_NAME = Input_town_map_name_from( argv[1] )
+
 VTYPE_FOLDER = Path('./output/global')
 VTYPE_FOLDER.mkdir(exist_ok=True, parents=True)
 NET_FOLDER = Path('./output/net')
 NET_FOLDER.mkdir(exist_ok=True, parents=True)
+
+_obj = sp.run(['bash', '-c', 'ps -a | grep CarlaUE4'], capture_output=True)
+if b'CarlaUE4' not in _obj.stdout:
+    print('You must run one CarlaUE4 instance first!')
+    exit(0)
 
 #========================== ==========================#
 vtype_args = vtypes.getArgParser().parse_args([
